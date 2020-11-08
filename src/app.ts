32c0f9b1699ff7741922d985027af9a65532ae4d
Bruno Henrique Santos <brunohenriquesantos272@gmail.com>
2020-11-07 22:23:42 -0300

    Inital Commit

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import 'reflect-metadata';
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use((request, response, next) => {
+  const error = new Error('Not found (404)');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, request, response, next) => {
+  response.status(error.status || 500);
+  response.json({ Error: error.message });
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,4 @@
+// Comecei as 22:00 07/11/2020
+import app from './app';
+
+app.listen(3000, () => console.log('> Server running'));
